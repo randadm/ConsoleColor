b4c1c9f145722b7fd026a9406b368973646be4b7
randadm <no-banana@void>
2023-08-03 09:57:54 +0200

    bug, maybe masking wasnt such a bad idea  after all..?

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ using System.Text.RegularExpressions;
 
 var console = new ConsoleHelper(RegexOptions.IgnoreCase);
 
-console.Rules.Add(new Rule("Comments", "\\/\\/.*\r", "green"));
+console.Rules.Add(new Rule("Comments", "\\/\\/.*(\r|$)", "darkgreen"));
+console.Rules.Add(new Rule("Numbers", "\\d+", "cyan"));
+console.Rules.Add(new Rule("url+emails", "((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w\\-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[.\\!\\/\\\\w]*))?)", "red"));
 
 /* 
  * 
@@ -15,5 +17,5 @@ console.Rules.Add(new Rule("Comments", "\\/\\/.*\r", "green"));
  */
 
 
-string text = File.ReadAllText(@"..\..\..\ConsoleHelper.cs");
+string text = "//Hello, this is Randall Adamson, my contact number is 123-456-7890, you can reach me at randall_adamson@email.com, and check my profile at www.randalladamson.com for more information.";  File.ReadAllText(@"..\..\..\ConsoleHelper.cs");
 console.WriteLine(text);
